97457a1fa006f66e6424b27eceb3dc7f09fddab7
AmxnMehra <amanmehra0112233@gmail.com>
2025-04-16 20:45:46 +0530

    damn

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+-- Typesctipt/javascript
+local ts_grp = vim.api.nvim_create_augroup("TSAutocmds", {})
+-- LspRestart eslint on saving package.json
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = { "package.json" },
+  command = "LspRestart eslint",
+  group = ts_grp,
+})
