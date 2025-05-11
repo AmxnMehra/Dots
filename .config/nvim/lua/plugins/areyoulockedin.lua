bd773afcad59cf5f6a8a1b594e871d581ea74a23
AmxnMehra <amanmehra0112233@gmail.com>
2025-05-11 18:44:45 +0530

    added modesPlugin

--- .config/nvim/lua/plugins/areyoulockedin.lua
+++ .config/nvim/lua/plugins/areyoulockedin.lua
@@ -4,7 +4,7 @@ return {
   event = "VeryLazy",
   config = function()
     require("areyoulockedin").setup({
-      session_key = "dfdfeecf-174a-4952-9086-81aa0238b88e",
+      session_key = "dfdfeecf-174a-4952-9086-81aa0238b88e", --get the session_key from the areyoulockedin.com
     })
   end,
 }
